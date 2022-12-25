d288aed42770fa245e3a505a3621fe10ea652fdd
Kunj Ahuja <88039115+Kunj-Ahuja@users.noreply.github.com>
2022-12-25 16:47:38 +0530

    nothing

--- check.py
+++
@@ -1,8 +0,0 @@
-a = 10
-for i in range(0,a+1,2):
-    print(i)
-print('--------------')
-
-while a>=0:
-    print(a)
-    a = a-2
--- projects/spotify downloader/spotify/__init__.py
+++
@@ -1,4 +0,0 @@
-from spotify.playlist import playlist
-from spotify.track import track
-from spotify.sp import sp
-from spotify.download import download
